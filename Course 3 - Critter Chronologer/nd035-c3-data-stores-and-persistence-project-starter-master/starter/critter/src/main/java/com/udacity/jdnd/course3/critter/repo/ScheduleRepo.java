eed28030a0c907cf042dd5a5e2301380833b906f
ReeceMccormack <122635919+ReeceMccormack@users.noreply.github.com>
2023-05-02 17:43:58 +0100

    Delete Course 3 - Critter Chronologer/nd035-c3-data-stores-and-persistence-project-starter-master directory

--- Course 3 - Critter Chronologer/nd035-c3-data-stores-and-persistence-project-starter-master/starter/critter/src/main/java/com/udacity/jdnd/course3/critter/repo/ScheduleRepo.java
+++
@@ -1,9 +0,0 @@
-package com.udacity.jdnd.course3.critter.repo;
-
-import com.udacity.jdnd.course3.critter.entity.Schedule;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ScheduleRepo extends JpaRepository<Schedule, Long> {
-}
